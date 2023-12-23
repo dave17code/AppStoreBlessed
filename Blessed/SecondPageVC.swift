b95f91116f0933955f6903b09b603926a7556678
dave17code <dave17code@gmail.com>
2023-12-23 16:36:17 +0900

    20231223Second

--- Blessed/SecondPageVC.swift
+++ Blessed/SecondPageVC.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class SecondPageVC: UIViewController {
-
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordChapterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        wordLabel.font = UIFont(name: "Copperplate", size: 24)
+        wordLabel.textAlignment = .center
+        wordLabel.numberOfLines = 0
+        
+        wordChapterLabel.font = UIFont(name: "Copperplate", size: 16)
+        wordChapterLabel.textAlignment = .center
+        
+        wordLabel.text = WordData().words[1]
+        wordChapterLabel.text = WordData().wordChapter[1]
     }
+    
+    
+    
+    
 }
--- Blessed/ThirdPageVC.swift
+++ Blessed/ThirdPageVC.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ThirdPageVC: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordChapterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        wordLabel.font = UIFont(name: "Copperplate", size: 24)
+        wordLabel.textAlignment = .center
+        wordLabel.numberOfLines = 0
+        
+        wordChapterLabel.font = UIFont(name: "Copperplate", size: 16)
+        wordChapterLabel.textAlignment = .center
+        
+        wordLabel.text = WordData().words[2]
+        wordChapterLabel.text = WordData().wordChapter[2]
     }
 }
