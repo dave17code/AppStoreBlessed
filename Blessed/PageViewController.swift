c64c94ca5041538142ebf938b54bbdb288b7069b
dave17code <dave17code@gmail.com>
2023-12-24 00:56:40 +0900

    20231224First

--- Blessed/PageViewController.swift
+++ Blessed/PageViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class PageViewController: UIPageViewController {
+    
+ 
+    let pageControl: UIPageControl = UIPageControl().then {
+        _ in
+    }
     
     lazy var vcArray: [UIViewController] = {
         return [self.vcInstance(name: "FirstPageVC"),
@@ -17,13 +24,24 @@ class PageViewController: UIPageViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.dataSource = self
         self.delegate = self
         
-        // 첫 번째 페이지를 기본 페이지로 설정
         if let firstVC = vcArray.first {
             setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+        
+        view.addSubview(pageControl)
+        
+        pageControl.numberOfPages = vcArray.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        
+        pageControl.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(170)
+            $0.centerX.equalToSuperview()
         }
     }
     
@@ -37,7 +55,9 @@ extension PageViewController: UIPageViewControllerDataSource, UIPageViewControll
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
         // 배열에서 현재 페이지의 컨트롤러를 찾아서 해당 인덱스를 현재 인덱스로 기록
-        guard let vcIndex = vcArray.firstIndex(of: viewController) else { return nil }
+        guard let vcIndex = vcArray.firstIndex(of: viewController) else {
+            return nil
+        }
         
         // 이전 페이지 인덱스
         let prevIndex = vcIndex - 1
@@ -45,9 +65,6 @@ extension PageViewController: UIPageViewControllerDataSource, UIPageViewControll
         // 인덱스가 0 이상이라면 그냥 놔둠
         guard prevIndex >= 0 else {
             return nil
-            
-            // 무한반복 시 - 1페이지에서 마지막 페이지로 가야함
-            // return vcArray.last
         }
         
         // 인덱스는 vcArray.count 이상이 될 수 없음
@@ -59,19 +76,21 @@ extension PageViewController: UIPageViewControllerDataSource, UIPageViewControll
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         
         guard let vcIndex = vcArray.firstIndex(of: viewController) else { return nil }
-            
-            // 다음 페이지 인덱스
-            let nextIndex = vcIndex + 1
-            
-            guard nextIndex < vcArray.count else {
-                return nil
-                
-                // 무한반복 시 - 마지막 페이지에서 1 페이지로 가야함
-                // return vcArray.first
-            }
-            
-            guard vcArray.count > nextIndex else { return nil }
-            
-            return vcArray[nextIndex]
+        
+        // 다음 페이지 인덱스
+        let nextIndex = vcIndex + 1
+        
+        guard nextIndex < vcArray.count else {
+            return nil
+        }
+        
+        guard vcArray.count > nextIndex else { return nil }
+        
+        return vcArray[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = vcArray.firstIndex(of: pageContentViewController)!
     }
 }
